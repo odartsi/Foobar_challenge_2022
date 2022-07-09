f72ff069849a62cca6881b1f53182195235fc400
Olympia Dartsi <58295268+odartsi@users.noreply.github.com>
2022-07-09 14:58:27 +0200

    Update and rename solution1.py to challenge_1.py

--- Level 1/solution1.py
+++
@@ -1,6 +0,0 @@
-def solution(data, n): 
-    from collections import Counter
-    counting_set= Counter(data)
-    x=[key for key,value in counting_set.items() if value>n]
-    data=list(filter(lambda i : i not in x, data)) 
-    return data
